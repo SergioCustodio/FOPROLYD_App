909f07064793fabbc97970f66f9daafff0785f81
kasuyaazura <44280383+kasuyaazura@users.noreply.github.com>
2020-01-14 09:12:05 -0600

    dbScript
    Creacion de Script de base de datos para Documentación

--- DataBase/DBscript.sql
+++ DataBase/DBscript.sql
@@ -29,6 +29,9 @@ insert into llamadas (Rubro_llamada,Fecha_llamada,Identificador,Categoria) value
 select Rubro_llamada, COUNT(Rubro_llamada) AS Cantidad FROM llamadas
 GROUP BY Rubro_llamada;
 
+Select Categoria, COUNT(Categoria) As Cantidad from llamadas
+group by Categoria;
+
 
 Create table correspondencias(
 ID_correspondencia int not null IDENTITY PRIMARY KEY,
@@ -52,6 +55,13 @@ audiencia_main varchar(50),
 observaciones_a varchar(350)
 );
 
+create table atencion(
+ID_atencion int not null IDENTITY PRIMARY KEY,
+Rubro_atencion varchar(25) not null,
+Tipo_atencion varchar(15)not null,
+Observacion_atencion varchar(350),
+);
+
 --alter table audiencias add Identificador int not null;
 
 
@@ -60,4 +70,12 @@ select ID_a as ID, Rubro_a as Rubro, Fecha_a as Fecha, Hora_a as Hora, nombre_a 
 
 select * from audiencias
 
-Truncate Table audiencias
+select * from correspondencias
+
+sELECT * FROM llamadas
+
+select * from atencion
+
+select ID_atencion as 'ID', Rubro_atencion as 'Rubro', Fecha_atencion as 'Fecha', Tipo_atencion as 'Tipo', Observacion_atencion as 'Observación' from atencion
+
+insert into atencion (Rubro_atencion , Tipo_atencion,Observacion_atencion,Fecha_atencion) values ('Retiro de Medicamentos','Beneficiario','El Beneficiario fulano vino por sus medicamentos','09/10/2019 2:51:25');
